443f5cbf79b5670c176ccafe597fb9d87428c54c
Joseph Toulier Pighi <jtoulier@gmail.com>
2021-10-05 16:07:48 -0500

    Update MainController.java

--- src/main/java/com/springonly/health/controller/MainController.java
+++ src/main/java/com/springonly/health/controller/MainController.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ public class MainController {
     @Autowired
     private InfoService infoService;
 
-    @GetMapping("/info")
+    @PostMapping("/info")
     public ResponseEntity<InfoResponse> getInfo(
             @RequestBody InfoRequest infoRequest
     ) {
